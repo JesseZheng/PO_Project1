e171ff6d9803a577628caba7cf4e720ac268c371
JesseZheng <653135740@qq.com>
2019-10-08 20:54:15 +0200

    Initiation and define Disk, CPU class.

---
+++ PO_Project1.py
@@ -0,0 +1,24 @@
+# Performance Optimization Project 1
+
+
+class Disk:
+
+    def __init__(self, name, servD):
+        self.name = name
+        self.servD = servD
+        self.qLength = 0
+
+
+class CPU:
+
+    def __init__(self, name, servD):
+        self.name = name
+        self.servD = servD
+        self.qLength = 0
+
+        
+
+
+
+
+
